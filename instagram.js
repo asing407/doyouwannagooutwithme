79a29249e8b3f5894813a4a4b696d9b9b0cc386c
Anshumaan Singh <144586980+asing407@users.noreply.github.com>
2024-03-03 17:59:29 +0530

    commit changes made

---
+++ instagram.js
@@ -0,0 +1,5 @@
+function getInstagramID() {
+    var instagramID = document.getElementById("instagram").value;
+    console.log("Instagram ID: " + instagramID);
+    return false; // prevent the form from submitting
+  }
